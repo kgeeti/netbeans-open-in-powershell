c0182d5468a3f21b69370bcfc508af8efa5ab410
KGe <kge@terra.com.br>
2025-06-13 16:37:09 -0300

    Sign modules

---
+++ target/generated-sources/annotations/br/eti/kge/openinpowershell/Bundle.java
@@ -0,0 +1,12 @@
+package br.eti.kge.openinpowershell;
+/** Localizable strings for {@link br.eti.kge.openinpowershell}. */
+class Bundle {
+    /**
+     * @return <i>Open in Powershell</i>
+     * @see OpenPowershellHere
+     */
+    static String CTL_OpenPowershellHere() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_OpenPowershellHere");
+    }
+    private Bundle() {}
+}
